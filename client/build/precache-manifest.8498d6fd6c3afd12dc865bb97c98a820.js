19c4ac7e5eb90a29699ac39d9cf855d8f316c71c
Chayut Aroonsang <59070035@it.kmitl.ac.th>
2020-08-19 14:41:05 +0700

    l

---
+++ client/build/precache-manifest.8498d6fd6c3afd12dc865bb97c98a820.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ba9b4576ad614fd8c0f68b313e6e98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5595b52d2c9cd397dd02",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f7f5360047bf8d12732b",
+    "url": "/static/css/main.9a1cd3d0.chunk.css"
+  },
+  {
+    "revision": "5595b52d2c9cd397dd02",
+    "url": "/static/js/2.b0432ad5.chunk.js"
+  },
+  {
+    "revision": "2abf98313037828bd96685fcb1ec3c23",
+    "url": "/static/js/2.b0432ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f5360047bf8d12732b",
+    "url": "/static/js/main.63ddce8a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c37d63559b1ca71fd4d75438cdd292e3",
+    "url": "/static/media/Resume.c37d6355.png"
+  },
+  {
+    "revision": "9b1e2a7871f5c3675601d516b3942059",
+    "url": "/static/media/profile.9b1e2a78.png"
+  },
+  {
+    "revision": "94498c5077a423e4d2d5e4128c3cfa2e",
+    "url": "/static/media/qrcode.94498c50.png"
+  }
+]);
